90e1e6ed7a1e92256d24371deea6c5178aa51a83
Jonathan Nguyen <jonathannguyen53@gmail.com>
2026-03-31 23:27:57 -0700

    Added Java - Bro Code Notes.org

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String [] args){
-        
+        System.out.println("\\I love pizza");
+        System.out.println("It's really good!");
+        System.out.println();
+        System.out.println();
     }
 }
+i
